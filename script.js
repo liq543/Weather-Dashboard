3f1c6f6cfee18f0a81b3a89816570efda4c87b4f
Griffin <liq543@gmail.com>
2023-07-14 21:50:25 -0500

    Everything working

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-// Replace 'your_api_key' with your actual API key from OpenWeatherMap
-var APIKey = '9d2557e2d99b2aa1f1c3bf11655c32e6';
+var APIKey = 'fcafd102401c66de2b3db010da96e87c';
 
-// List of predefined city buttons
-var cityButtons = ['Atlanta', 'Denver', 'Seattle', 'San Francisco', 'Orlando', 'New York', 'Chicago', 'Austin'];
-
-// Function to fetch and display weather data
 function getWeather(city) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`)
     .then(response => response.json())
     .then(data => {
-      // Display current weather
       document.getElementById('currentWeather').innerHTML = `
         <h2>${data.name} (${new Date().toLocaleDateString()})</h2>
         <p>Temperature: ${data.main.temp} °F</p>
@@ -17,11 +11,9 @@ function getWeather(city) {
         <p>Humidity: ${data.main.humidity} %</p>
       `;
 
-      // Get future weather
       fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIKey}`)
         .then(response => response.json())
         .then(data => {
-          // Display future weather
           var futureWeatherDiv = document.getElementById('futureWeather');
           futureWeatherDiv.innerHTML = '<h2>5-Day Forecast:</h2>';
           for (let i = 0; i < data.list.length; i += 8) {
@@ -38,17 +30,15 @@ function getWeather(city) {
     });
 }
 
-// Function to handle city button click
 function handleCityButtonClick(event) {
   getWeather(event.target.textContent);
 }
 
-// Add event listeners to city buttons
-for (let i = 1; i <= cityButtons.length; i++) {
-  document.getElementById(`city${i}`).addEventListener('click', handleCityButtonClick);
+var cityButtons = document.getElementsByClassName('cityButton');
+for (var i = 0; i < cityButtons.length; i++) {
+  cityButtons[i].addEventListener('click', handleCityButtonClick);
 }
 
-// Add event listener to search button
 document.getElementById('searchButton').addEventListener('click', function(event) {
   event.preventDefault();
   var city = document.getElementById('city').value;
